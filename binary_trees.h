1ad9e64dd5945eeea58ae73e4d14ab98a86b24a1
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-01-07 08:20:55 +0300

    feat: BST - Insert

--- binary_trees.h
+++ binary_trees.h
@@ -21,6 +21,9 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
+/* Binary Search Tree */
+typedef struct binary_tree_s bst_t;
+
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -48,5 +51,6 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
 
 #endif /* _BINARY_TREES_H_ */
